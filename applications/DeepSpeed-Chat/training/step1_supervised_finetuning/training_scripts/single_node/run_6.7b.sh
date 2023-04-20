a520de26900716723c1981080a4923631d3eed94
minchangwei <minchangwei@tencent.com>
2023-04-21 00:13:00 +0800

    batch_size 8

--- applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/single_node/run_6.7b.sh
+++ applications/DeepSpeed-Chat/training/step1_supervised_finetuning/training_scripts/single_node/run_6.7b.sh
@@ -17,8 +17,8 @@ deepspeed main.py \
    --data_path Dahoas/rm-static Dahoas/full-hh-rlhf Dahoas/synthetic-instruct-gptj-pairwise yitingxie/rlhf-reward-datasets openai/webgpt_comparisons stanfordnlp/SHP \
    --data_split 2,4,4 \
    --model_name_or_path facebook/opt-6.7b \
-   --per_device_train_batch_size 2 \
-   --per_device_eval_batch_size 2 \
+   --per_device_train_batch_size 8 \
+   --per_device_eval_batch_size 8 \
    --max_seq_len 512 \
    --learning_rate 9.65e-6 \
    --weight_decay 0.1 \
